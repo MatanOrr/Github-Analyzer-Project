6ac426dd1c9d164eb7c9b6866d78c43497be08cc
MatanOrr <matan.orr@mail.huji.ac.il>
2023-11-02 19:50:24 +0200

    debugging

--- script/user-data.js
+++ script/user-data.js
@@ -4,11 +4,10 @@ export function createUserBox(data) {
     infoBoxDiv = document.getElementById("userInfoBox");
     infoBoxDiv.classList.add("info-box");
 
-    // Create user avatar
-    const userAvatar = document.createElement("img");
-    userAvatar.className = "avatar";
-    userAvatar.src = data.avatar_url;
-    infoBoxDiv.appendChild(userAvatar);
+    // Create user name
+    const userName = document.createElement("h2");
+    userName.innerText = data.name;
+    infoBoxDiv.appendChild(userName);
 
     // Create user login
     const userLogin = document.createElement("a");
@@ -16,14 +15,30 @@ export function createUserBox(data) {
     userLogin.href = data.html_url;
     userLogin.innerText = '@' + data.login;
     infoBoxDiv.appendChild(userLogin);
-}
 
-function getFollowStats(data, infoBoxDiv) {
+    //Create user description
+    const userDescription = document.createElement("p");
+    userDescription.innerText = data.bio;
+    infoBoxDiv.appendChild(userDescription);
+
+    // Create user avatar
+    const userAvatar = document.createElement("img");
+    userAvatar.className = "avatar";
+    userAvatar.src = data.avatar_url;
+    infoBoxDiv.appendChild(userAvatar);
+
     // Get the number of followers and following directly from the data object
     const followersNum = data.followers; // These should be provided as numbers
     const followingNum = data.following;
 
     const followStats = document.createElement("p");
-    followStats.innerText = `${followersNum} followers · ${followingNum} following`;
+    if (followersNum == 0 && followingNum == 0) {
+        followStats.innerText = 'No followers or following';
+    } else if (followersNum == 0) {
+        followStats.innerText = `${followingNum} following`;
+    } else if (followingNum == 0) {
+        followStats.innerText = `${followersNum} followers`;
+    } else
+        followStats.innerText = `${followersNum} followers | ${followingNum} following`;
     infoBoxDiv.appendChild(followStats);
 }
