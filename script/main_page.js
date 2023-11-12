0e5224dea877dbaec189694332915570302c9385
MatanOrr <matan.orr@mail.huji.ac.il>
2023-11-12 16:25:49 +0200

    Hadar Review  1.3

--- script/main_page.js
+++ script/main_page.js
@@ -1,34 +1,37 @@
-async function userExits(username) {
-    const userDataApi = `https://api.github.com/users/${username}`;
-    const response = await fetch(userDataApi);
-    if (response.ok) {
-        return true;
-    }
-    return false;
+function alertNonUser() {
+    const errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerText = "User does not exist";
 }
 
-function alertNonUser() {
-    const userNotFound = document.createElement("p");
-    userNotFound.innerText = "User not found";
-    userNotFound.className = "user-not-found";
-    searchBox.appendChild(userNotFound);
+async function checkUserExits(username) {
+    const userDataApi = `https://api.github.com/users/${username}`;
+    const response = await fetch(userDataApi);
+    const data = await response.json(); // Convert response to JSON
+    if (data.message == "Not Found") {
+        alertNonUser();
+        return false;
+    }
+    return true;
 }
+
 
 document.addEventListener("DOMContentLoaded", async function() {
     const searchBox = document.getElementById("usernameInput");
     const submitButton = document.getElementById("submitButton");
-    searchBox.addEventListener('keypress', function(e) {
+
+    searchBox.addEventListener('keypress', async function(e) { // Add async here
         if (e.key === 'Enter') {
             e.preventDefault();
-            if (!userExits(searchBox.value)) {
+            if (!await checkUserExits(searchBox.value)) { // Corrected function name
                 alertNonUser();
                 return;
             }
             window.location.href = `user_page.html?username=${searchBox.value}`;
         }
-    })
-    submitButton.addEventListener('click', function() {
-        if (!userExits(searchBox.value)) {
+    });
+
+    submitButton.addEventListener('click', async function() { // Add async here
+        if (!await checkUserExits(searchBox.value)) { // Corrected function name
             alertNonUser();
             return;
         }
